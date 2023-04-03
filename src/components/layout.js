f827a0e4ce65fa73639ec81665e69ae27c568e52
DEEPAKKUMAR E <deepak@Deepaks-MacBook-Air.local>
2023-04-03 19:42:30 +0530

    bug fix2

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,16 +6,16 @@ const Layout = ({ children, title, description, ogTitle, ogDescription, ogImage,
   return (
     <>
        <Head>
-        <title>{title==null? 'Deepak - Blog':title}</title>
-        <meta property="title" content={title==null?  'Deepak - Blog':title}/>
-        <meta property="description" content={description==null?  'Find interesting daily blog posts by Deepak':description} />
+        <title>{title}</title>
+        <meta property="title" content={title}/>
+        <meta property="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="url" content={ogUrl==null? 'https://blog.iam-deepak.me':ogUrl} />
+        <meta property="url" content={ogUrl} />
         <meta property="type" content="article" />
-        <meta property="site_name" content={title==null?  'Deepak - Blog':title} />
-        <meta property="og:title" content={ogTitle==null? 'Deepak - Blog':title} />
-        <meta property="og:description" content={ogDescription ==null? 'Find interesting daily blog posts by Deepak':description} />
+        <meta property="site_name" content={title} />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
         <meta property="og:image" content={ogImage} />
       </Head>
       <Header />
